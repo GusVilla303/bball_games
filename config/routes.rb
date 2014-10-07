0749a424320a034e32f09f56a773a355cb9f81a7
GusVilla303 <gvillagrana@outlook.com>
2014-10-07 12:51:55 -0600

    Create a new User show page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users,       only: [:new, :create, :show, :edit, :update] do
+    member do
+      get :dashboard
+      get :avatar
+    end
+  end
 
   resources :welcome
   root to: 'welcome#index'
   get 'auth/twitter/callback', to: 'sessions#create', as: 'signin'
-  #get 'auth/failure', to: redirect('/')
+  get 'auth/failure', to: redirect('/')
   get 'signout', to: 'sessions#destroy', as: 'signout'
+
+
+  get 'dashboard', to: 'users#dashboard', as: 'dashboard'
 end
