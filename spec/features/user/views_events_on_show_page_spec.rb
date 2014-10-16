e6e6473b08f4d4af1aecf43d162addb7a056a4ed
GusVilla303 <gvillagrana@outlook.com>
2014-10-15 21:25:48 -0600

    Add event description to user-show-view

--- spec/features/user/views_events_on_show_page_spec.rb
+++ spec/features/user/views_events_on_show_page_spec.rb
@@ -1,12 +1,11 @@
 require "spec_helper"
-require "rails_helper"
 
 RSpec.feature "User can view" do
 
   scenario "events on their show page" do
-    user  = User.new(id: 1, first_name: "Gustavo")
-    event = Event.new(description: "3 on 3")
-    visit user_path
+    user  = User.create!(first_name: "Gustavo")
+    event = Event.create!(description: "3 on 3")
+    visit user_path(user.id)
     expect(page).to have_content("Gustavo")
     expect(page).to have_content("3 on 3")
   end
