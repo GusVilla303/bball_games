6d75d069fda2c777780b66284bc54e1b3471823c
GusVilla303 <gusvilla303@gmail.com>
2015-02-05 10:23:38 -0700

    Start the clean up process. Since the app consists of only TWO pages total, I won't need all the views and bootstrap files.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,18 +3,9 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @users }
-    end
   end
 
   def show
-    respond_to do |format|
-      format.html
-      format.json { render json: @user }
-    end
   end
 
   def new
@@ -26,16 +17,6 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render json: @user, status: :created }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   def update
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,6 +1,6 @@
 class Event < ActiveRecord::Base
   has_many :user_events
   has_many :users, through: :user_events
-  #
+  
   belongs_to :location
 end
